44b35a90b0eda0eba89f5c676ec21312159f3879
Renato Xavier <you@example.com>
2022-05-22 12:11:59 -0300

    date times for timestamps

--- rxAppBM/Domain/Entities/BlueMeteringMessage.cs
+++ rxAppBM/Domain/Entities/BlueMeteringMessage.cs
@@ -6,9 +6,11 @@ namespace rxAppBM.Domain.Entities
     {
         public Guid BlueMeteringMessageId { get; set; }
         public double ParamsRxTime { get; set; }
+        public DateTime? ParamsRxTimeDateTime { get; set; }
         public int ParamsPort { get; set; }
         public bool ParamsDuplicate { get; set; }
         public long ParamsRadioGpsTime { get; set; }
+        public DateTime? ParamsRadioGpsTimeDateTime { get; set; }
         public double ParamsRadioDelay { get; set; }
         public int ParamsRadioDataRate { get; set; }
         public long ParamsRadioModulationBandwidth { get; set; }
@@ -18,6 +20,7 @@ namespace rxAppBM.Domain.Entities
         public int ParamsRadioHardwareStatus { get; set; }
         public int ParamsRadioHardwareChain { get; set; }
         public long ParamsRadioHardwareTmst { get; set; }
+        public DateTime? ParamsRadioHardwareTmstDateTime { get; set; }
         public double ParamsRadioHardwareSnr { get; set; }
         public double ParamsRadioHardwareRssi { get; set; }
         public double ParamsRadioHardwareChannel { get; set; }
@@ -25,6 +28,7 @@ namespace rxAppBM.Domain.Entities
         public double ParamsRadioHardwareGpsLng { get; set; }
         public int ParamsRadioHardwareGpsAlt { get; set; }
         public double ParamsRadioTime { get; set; }
+        public DateTime? ParamsRadioTimeDateTime { get; set; }
         public double ParamsRadioFrequency { get; set; }
         public int ParamsRadioSize { get; set; }
         public int ParamsCounterUp { get; set; }
@@ -42,6 +46,7 @@ namespace rxAppBM.Domain.Entities
         public string MetaApplication { get; set; }
         public string MetaDeviceAddr { get; set; }
         public double MetaTime { get; set; }
+        public DateTime? MetaTimeDateTime { get; set; }
         public string MetaDevice { get; set; }
         public string MetaPacketId { get; set; }
         public string MetaGateway { get; set; }
